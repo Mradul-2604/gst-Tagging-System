ef93f0c0c1f57769edb731715a03ca1b5e3898fa
Mradul <mradulmahajan@gmail.com>
2026-03-31 15:32:20 +0530

    changes

--- app.py
+++ app.py
@@ -263,4 +263,6 @@ def download_excel():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
